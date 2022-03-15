17ffe99e98391efa469750d743f435c935c0dcea
jonathan <jonahoffis@gmail.com>
2022-03-15 16:02:27 +0100

    faktisk et spill atm

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,24 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'other_page.dart';
 
-void main() => runApp(MyApp());
+const app = MyApp();
+
+void main() {
+  runApp(const MaterialApp(home: app));
+}
 
 class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   State<StatefulWidget> createState() => _FavoriteWidgetState();
 }
 
+bool _pressedSuicide = false;
+
 class _FavoriteWidgetState extends State<MyApp> {
-
-  bool presss = false;
-
   final ButtonStyle raisedButtonStyle = ElevatedButton.styleFrom(
     onPrimary: Colors.black87,
     primary: Colors.grey[300],
@@ -22,53 +29,114 @@ class _FavoriteWidgetState extends State<MyApp> {
     ),
   );
 
-  Text jackText = Text("This is Jack. He's mad.");
+  Text jackText = const Text("This is Jack. He's mad.");
 
   @override
   Widget build(BuildContext context) {
-    if (!presss)
-      return MaterialApp(
-          home: Scaffold(
-            appBar: AppBar(
-              title: Text("Imaggeee"),
+    if (!_pressedSuicide) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("Imaggeee"),
+        ),
+        body: Wrap(children: <Widget>[
+          Image.asset(
+              'assets/images/movie-300-helmet-shield-wallpaper-thumb.jpg'),
+          jackText,
+          SizedBox(
+            width: 200,
+            child: TextButton(
+              style: TextButton.styleFrom(
+                  primary: Colors.blue, backgroundColor: Colors.amberAccent),
+              onPressed: () {
+                setState(() {
+                  _pressedSuicide = !_pressedSuicide;
+                });
+              },
+              child: const Text('Fight him.'),
             ),
-            body: Wrap(children: <Widget>[
-              Image.asset('assets/images/movie-300-helmet-shield-wallpaper-thumb.jpg'),
-              jackText,
-              Container(
-                width: 200,
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                      primary: Colors.blue,
-                      backgroundColor: Colors.amberAccent
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      presss = !presss;
-                    });
-                  },
-                  child: Text('Fight him.'),
-                ),
-              ),
-              ElevatedButton(
-                style: raisedButtonStyle,
-                onPressed: () {
-                  if (presss) {
-                    jackText = Text(super.toString());
-                  } else {
-                    jackText = const Text("eyoooo");
-                  }
-                },
-                child: Text('Looks like I have to go.'),
-              )
-
-            ]),
-            bottomNavigationBar: (presss ? const BottomAppBar(
-                child: Text("Cam on ingerland! Score some faking goals!! ENG 7-1 BRA")
-            ) : Text("asd")),
+          ),
+          ElevatedButton(
+            style: raisedButtonStyle,
+            onPressed: () {
+              Navigator.push(context, _createRoute(() {
+                setState(() {});
+              }));
+            },
+            child: const Text('Go around him because you\'re weak'),
           )
+        ]),
+        bottomNavigationBar: (!_pressedSuicide
+            ? const BottomAppBar(
+                child: Text(
+                    "Cam on ingerland! Score some faking goals!! ENG 7-1 BRA"))
+            : const Text("asd")),
       );
-    else
-      return new OtherPage();
+    } else {
+      return DiedPage();
+    }
+  }
+}
+
+Route _createRoute(Function() updateFunc) {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) =>
+        MapScene(updateFunc: updateFunc),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      return child;
+    },
+  );
+}
+
+class MapScene extends StatelessWidget {
+  final Function() updateFunc;
+
+  const MapScene({Key? key, required this.updateFunc}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Second Route'),
+        ),
+        body: Container(
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage(
+                      'assets/images/1a4314_47bd8f4a28014766a9a4a9c2ec051eac~mv2.jpg'),
+                  fit: BoxFit.cover)),
+          child: ListView(children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Face Jack again!'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+                _pressedSuicide = true;
+                updateFunc();
+              },
+              child: const Text('Drink from that weird brown bottle'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                var ran = Random();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => PracticePage(
+                            success: ran.nextBool(),
+                            context: context,
+                          )),
+                );
+              },
+              child: const Text('Practice your skills brah'),
+            ),
+          ]),
+          constraints: const BoxConstraints.expand(),
+          // width: double.infinity,
+          // height: 500,
+        ));
   }
 }
--- lib/other_page.dart
+++ lib/other_page.dart
@@ -1,12 +1,38 @@
-
 import 'package:flutter/material.dart';
 
-class OtherPage extends MaterialApp {
-  const OtherPage({Key? key}) : super(key: key,
-  home: const Scaffold(
-    body: Center(child: const Text("hei")),
-  )
-  );
+var skills = 0;
 
-
+class DiedPage extends MaterialApp {
+  DiedPage({Key? key})
+      : super(
+            key: key,
+            home: Scaffold(
+              body: Center(
+                  child: Text(skills < 10
+                      ? "You died,\n and now you're stuck in the underworld."
+                      : "YOU WON!!!")),
+            ));
 }
+
+class PracticePage extends MaterialApp {
+  PracticePage({Key? key, required bool success, required BuildContext context})
+      : super(
+            key: key,
+            home: GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+                child: Scaffold(
+              body: Center(
+                  child: Text(success
+                      ? "+1 You SMASHED your opponent"
+                      : "You didn't smash... ;-;")),
+              backgroundColor: success
+                  ? const Color.fromRGBO(105, 234, 105, 1.0)
+                  : const Color.fromRGBO(236, 55, 49, 1.0),
+            ))) {
+    skills++;
+  }
+}
+
+class RouteMine extends Route {}
